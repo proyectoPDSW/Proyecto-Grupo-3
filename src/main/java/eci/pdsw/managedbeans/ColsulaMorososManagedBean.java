d0ad72d976f1564228d93f8974221c002a05300a
Julian Devia <judadese@gmail.com>
2016-04-19 16:03:58 -0500

    vista de consulta y servicios prestamos iniciados

---
+++ src/main/java/eci/pdsw/managedbeans/ColsulaMorososManagedBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eci.pdsw.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author 2105684
+ */
+@ManagedBean(name="ConsulaMorosos")
+@SessionScoped
+public class ColsulaMorososManagedBean {
+    
+    
+}
---
+++ src/main/java/eci/pdsw/servicios/ServiciosPrestamo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eci.pdsw.servicios;
+
+/**
+ *
+ * @author 2105684
+ */
+public abstract class ServiciosPrestamo {
+    
+    private static final ServiciosPrestamo instance=new ServiciosPrestamoDAO();
+    
+}
