10189bab6fb7b0b4d3d53339172570d4711d3d96
daniela sepulveda <daniela.sepulveda@mail.escuelaing.edu.co>
2016-04-21 10:07:34 -0500

    vista

--- src/main/java/eci/pdsw/managedbeans/RegistroEquipoComplejoManagedBean.java
+++ src/main/java/eci/pdsw/managedbeans/RegistroEquipoComplejoManagedBean.java
@@ -8,9 +8,13 @@ package eci.pdsw.managedbeans;
 import com.mysql.jdbc.Blob;
 import com.mysql.jdbc.BlobFromLocator;
 import eci.pdsw.entities.Modelo;
+import eci.pdsw.servicios.ExcepcionServicios;
+import eci.pdsw.servicios.ServiciosEquipoComplejo;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -25,10 +29,15 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 
 public class RegistroEquipoComplejoManagedBean implements Serializable{
+    
+    private final ServiciosEquipoComplejo servicios=ServiciosEquipoComplejo.getInstance();
         
     private String nombreModelo;
     private Modelo modelo;
+     
 
+    private boolean showPassword = true;
+    
     public Blob getFotografia() {
         return modelo.getFotografia();
     }
@@ -46,10 +55,20 @@ public class RegistroEquipoComplejoManagedBean implements Serializable{
     }
     
     public void consultarModelo(){
-        modelo=new Modelo(55, "model",null, "asda", new Long("54654"));
-        if(!nombreModelo.equals(modelo.getNombre())){
-           facesError("No se ha encontrado el modelo");
+        try {
+            servicios.consultarPorModelo(nombreModelo);
+        } catch (ExcepcionServicios ex) {
+            facesError("");
+            Logger.getLogger(RegistroEquipoComplejoManagedBean.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+    }
+    public void registrarModelo(){
+        
+    }
+     
+    public boolean isShowPassword(){
+        return showPassword;
     }
     /**
      * Adds a new SEVERITY_ERROR FacesMessage for the ui
