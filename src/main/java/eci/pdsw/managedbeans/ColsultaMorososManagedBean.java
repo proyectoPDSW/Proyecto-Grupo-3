90b0f73530cbe88854336c73e4c126ed34ded9bf
Julian Devia <judadese@gmail.com>
2016-04-21 10:09:14 -0500

    vista consultar morosos

--- src/main/java/eci/pdsw/managedbeans/ColsultaMorososManagedBean.java
+++ src/main/java/eci/pdsw/managedbeans/ColsultaMorososManagedBean.java
@@ -6,9 +6,13 @@
 package eci.pdsw.managedbeans;
 
 import eci.pdsw.entities.EquipoComplejo;
+import eci.pdsw.entities.Modelo;
+import eci.pdsw.entities.Persona;
 import eci.pdsw.entities.Prestamo;
+import eci.pdsw.entities.PrestamoTerminoFijo;
 import eci.pdsw.servicios.ServiciosPrestamo;
-import java.util.Date;
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -21,7 +25,7 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name="consultaMorosos")
 @SessionScoped
 public class ColsultaMorososManagedBean {
-    private final ServiciosPrestamo sp=ServiciosPrestamo.getInstance();
+    //private final ServiciosPrestamo sp=ServiciosPrestamo.getInstance();
     
     private List<Prestamo> morosos;
 
@@ -29,12 +33,20 @@ public class ColsultaMorososManagedBean {
      * @return the morosos
      */
     public List<Prestamo> getMorosos() {
-        morosos=sp.consultarPrestamosMorosos();
+        Persona per=new Persona("2105684", "Julian", "Devia", "yo@asdads.com", "1234567");
+        Modelo lod=new Modelo(5, "yonose", null, "grande", 1234);
+        EquipoComplejo ec2=new EquipoComplejo(lod, "esta", "132456789");
+        EquipoComplejo ec1=new EquipoComplejo(lod, "otra", "213456789");
+        List<EquipoComplejo> pres=new LinkedList<>();
+        List<EquipoComplejo> falt=new LinkedList<>();
+        pres.add(ec1);pres.add(ec2);falt.add(ec2);
+        Prestamo p= new PrestamoTerminoFijo(per, pres, null, new Date(Calendar.getInstance().getTime().getTime()));
+        morosos=new LinkedList<>();morosos.add(p);
         return morosos;
     }
     
     public Date diffDate(Date d){
-        return new Date(new Date().getTime()-d.getTime());
+        return new Date(new Date(Calendar.getInstance().getTime().getTime()).getTime()-d.getTime());
     }
     
 
