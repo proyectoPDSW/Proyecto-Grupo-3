a79e48b2c33638e6410e9b8fdedaf99594316957
David Esteban Useche <david.useche@mail.escuelaing.edu.co>
2016-04-21 15:33:07 -0500

    Documentacion DAO y servicios

--- src/main/java/eci/pdsw/persistence/DAOEquipoComplejo.java
+++ src/main/java/eci/pdsw/persistence/DAOEquipoComplejo.java
@@ -15,21 +15,70 @@ import eci.pdsw.entities.Modelo;
  */
 public interface DAOEquipoComplejo {
 
+    /**
+     * Carga un equipo complejo de la base de datos por serial
+     * @param serial a buscar
+     * @return El equipo complejo cargado
+     * @throws PersistenceException si no encontro el serial
+     */
     public EquipoComplejo load(String serial) throws PersistenceException;
 
+    /**
+     * Carga un equipo complejo de la base de datos por placa
+     * @param placa a buscar
+     * @return El equipo complejo cargado
+     * @throws PersistenceException si no encontro la placa
+     */
     public EquipoComplejo load(int placa) throws PersistenceException;
 
+    /**
+     * Guarda un equipo complejo en la base de datos
+     * @param toSave Equipo a guardar
+     * @throws PersistenceException si el equipo ya esta registrado
+     */
     public void save(EquipoComplejo toSave) throws PersistenceException;
 
+    /**
+     * Actualiza un equipo en la base de datos
+     * @param toUpdate el equipo actualizado
+     * @throws PersistenceException si el equipo no existe
+     */
     public void update(EquipoComplejo toUpdate) throws PersistenceException;
 
+    /**
+     * Reemplaza un equipo por otro en la base de datos
+     * @param old el equipo a reemplazar
+     * @param novo el equipo que lo reemplaza
+     * @throws PersistenceException si el equipo a reemplazar no existe
+     */
     public void reemplazar(EquipoComplejo old, EquipoComplejo novo) throws PersistenceException;
 
+    /**
+     * Carga todos los equipos complejos de la base de datos
+     * @return Una lista que contiene todos los equipos
+     * @throws PersistenceException Si hay algun error SQL
+     */
     public ArrayList<EquipoComplejo> loadAll() throws PersistenceException;
 
+    /**
+     * Carga todos los equipos en la base de datos que puedan prestarse
+     * @return Una lista con los equipos complejos disponibles
+     * @throws PersistenceException Si hay algun error SQL
+     */
     public ArrayList<EquipoComplejo> loadDisponibles() throws PersistenceException;
 
+    /**
+     * Obtiene todos los equipos de un modelo
+     * @param modelo usado para buscar
+     * @return Una lista con los equipos que son del modelo
+     * @throws PersistenceException 
+     */
     public ArrayList<EquipoComplejo> loadByModelo(String modelo) throws PersistenceException;
     
+    /**
+     * Guarda un modelo en la base de datos
+     * @param model a guardar
+     * @throws PersistenceException si el modelo ya existe
+     */
     public void save(Modelo model) throws PersistenceException;
 }
--- src/main/java/eci/pdsw/persistence/DAOEquipoSencillo.java
+++ src/main/java/eci/pdsw/persistence/DAOEquipoSencillo.java
@@ -14,13 +14,40 @@ import eci.pdsw.entities.*;
  */
 public interface DAOEquipoSencillo {
 
+    /**
+     *Carga un equipo sencillo de la base de datos
+     * @param nombre del equipo sencillo a cargar
+     * @return el Equipo sencillo cargado
+     * @throws PersistenceException si el equipo no existe
+     */
     public EquipoSencillo load(String nombre) throws PersistenceException;
 
+    /**
+     * Guarda un equipo sencillo en la base de datos
+     * @param toSave el Equipo sencillo a guardar
+     * @throws PersistenceException si el Equipo sencillo ya existe
+     */
     public void save(EquipoSencillo toSave) throws PersistenceException;
 
+    /**
+     * Actualiza un equipo sencillo
+     * @param toUpdate el equipo sencillo actualizado
+     * @throws PersistenceException si el equipo sencillo no existe
+     */
     public void update(EquipoSencillo toUpdate) throws PersistenceException;
 
+    /**
+     * Carga todos los equipos sencillos de la base de datos
+     * @return Una lista con los equipos sencillos 
+     * @throws PersistenceException Si hay un error SQL
+     */
     public ArrayList<EquipoSencillo> loadAll() throws PersistenceException;
 
+    /**
+     * Carga la cantidad de elementos de un equipo sencillo disponibles para prestar
+     * @param nombre del Equipo sencillo a buscar
+     * @return el numero de elemenetos que se pueden prestar
+     * @throws PersistenceException si el Equipo sencillo no existe
+     */
     public int loadByNombreDisponibles(String nombre) throws PersistenceException;
 }
