67390cc0a8ab4ce3ace188a58a4a92854e615694
Julian Devia <julian.devia@mail.escuelaing.edu.co>
2016-04-20 23:40:14 -0500

    esqueleto servicios prestamo

--- src/main/java/eci/pdsw/servicios/ServiciosPrestamo.java
+++ src/main/java/eci/pdsw/servicios/ServiciosPrestamo.java
@@ -5,6 +5,11 @@
  */
 package eci.pdsw.servicios;
 
+import eci.pdsw.entities.EquipoComplejo;
+import eci.pdsw.entities.Persona;
+import eci.pdsw.entities.Prestamo;
+import java.util.List;
+
 /**
  *
  * @author 2105684
@@ -12,5 +17,33 @@ package eci.pdsw.servicios;
 public abstract class ServiciosPrestamo {
     
     private static final ServiciosPrestamo instance=new ServiciosPrestamoPersistence();
+
+    public static ServiciosPrestamo getInstance() throws RuntimeException{        
+        return instance;
+    }
+    /**
+     * Consulta todos los prestamos con algun equipo en mora
+     * @return los prestamos que posean algun equipo en mora de ser entregado
+     */
+    public abstract List<Prestamo> consultarPrestamosMorosos();
     
+    /**
+     * Consulta todos los prestamos de una persona determinada
+     * @param p la persona de la que se quieren conocer los prestamos
+     * @return Los prestamos de p
+     */
+    public abstract List<Prestamo> consultarPrestamosPersona(Persona p);
+    
+    /**
+     * Consulta todos los prestamos de un equipo complejo determinado
+     * @param ec el equipo complejo del que se quieren conocer los prestamos
+     * @return Los prestamos de ec
+     */
+    public abstract List<Prestamo> consultarPrestamosEquipoComplejo(EquipoComplejo ec);
+    
+    /**
+     * Consulta todos los prestamos
+     * @return todos los prestamos registrados
+     */
+    public abstract List<Prestamo> consultarTodos();
 }
--- src/main/java/eci/pdsw/servicios/ServiciosPrestamoPersistence.java
+++ src/main/java/eci/pdsw/servicios/ServiciosPrestamoPersistence.java
@@ -5,13 +5,60 @@
  */
 package eci.pdsw.servicios;
 
+import eci.pdsw.entities.EquipoComplejo;
+import eci.pdsw.entities.Persona;
+import eci.pdsw.entities.Prestamo;
+import eci.pdsw.persistence.DAOFactory;
+import eci.pdsw.persistence.DAOPrestamos;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 2105684
  */
-class ServiciosPrestamoPersistence extends ServiciosPrestamo {
+public class ServiciosPrestamoPersistence extends ServiciosPrestamo {
 
+    private DAOPrestamos basePaciente;
+    private DAOFactory daoF;
+    
     public ServiciosPrestamoPersistence() {
+    	try {
+            
+            InputStream input = getClass().getClassLoader().getResource("applicationconfig.properties").openStream();
+            
+            Properties properties=new Properties();
+            properties.load(input);
+            daoF = DAOFactory.getInstance(properties);
+          
+        } catch (IOException ex) {
+            Logger.getLogger(ServiciosPrestamoPersistence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public List<Prestamo> consultarPrestamosMorosos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Prestamo> consultarPrestamosPersona(Persona p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Prestamo> consultarPrestamosEquipoComplejo(EquipoComplejo ec) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Prestamo> consultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
