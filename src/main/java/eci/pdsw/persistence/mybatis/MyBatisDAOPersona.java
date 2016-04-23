40ce52b3e94605ad41c8921e01cb7a6edad17154
Hugo Alvarez <hugoferneya@hotmail.com>
2016-04-22 21:50:26 -0500

    correccion en el pom.xml

--- src/main/java/eci/pdsw/persistence/mybatis/MyBatisDAOPersona.java
+++ src/main/java/eci/pdsw/persistence/mybatis/MyBatisDAOPersona.java
@@ -8,13 +8,24 @@ package eci.pdsw.persistence.mybatis;
 import eci.pdsw.entities.Persona;
 import eci.pdsw.persistence.DAOPersona;
 import eci.pdsw.persistence.PersistenceException;
+import eci.pdsw.persistence.mybatis.mappers.PersonaMapper;
 import java.util.List;
+import org.apache.ibatis.session.SqlSession;
 
 /**
  *
  * @author Hugo Alvarez
  */
 public class MyBatisDAOPersona implements DAOPersona{
+    private SqlSession ses;
+    private PersonaMapper pmap;
+
+    public MyBatisDAOPersona(SqlSession ses) {
+        this.ses = ses;
+        pmap = this.ses.getMapper(PersonaMapper.class);
+    }
+    
+    
 
     @Override
     public Persona load(String carne) throws PersistenceException {
