c5c7d8eba96dcbf9243e981145174bbe05ab0669
Hugo Alvarez <hugoferneya@hotmail.com>
2016-04-24 00:52:38 -0500

    revisando algunos errores

--- src/main/java/eci/pdsw/persistence/mybatis/MyBatisDAOPrestamo.java
+++ src/main/java/eci/pdsw/persistence/mybatis/MyBatisDAOPrestamo.java
@@ -9,15 +9,23 @@ import eci.pdsw.entities.EquipoComplejo;
 import eci.pdsw.entities.Prestamo;
 import eci.pdsw.persistence.DAOPrestamo;
 import eci.pdsw.persistence.PersistenceException;
+import eci.pdsw.persistence.mybatis.mappers.PrestamoMapper;
 import java.sql.Timestamp;
 import java.util.List;
+import org.apache.ibatis.session.SqlSession;
 
 /**
  *
  * @author Hugo Alvarez
  */
 public class MyBatisDAOPrestamo  implements DAOPrestamo{
+    private SqlSession ses;
+    private PrestamoMapper pmap;
 
+    public MyBatisDAOPrestamo(SqlSession ses) {
+        this.ses = ses;
+        pmap = this.ses.getMapper(PrestamoMapper.class);
+    }
     @Override
     public List<Prestamo> load(Timestamp fecha, String carne) throws PersistenceException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
---
+++ src/main/java/eci/pdsw/persistence/mybatis/mappers/PrestamoMapper.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eci.pdsw.persistence.mybatis.mappers;
+
+/**
+ *
+ * @author Hugo Alvarez
+ */
+public interface PrestamoMapper {
+    
+}
