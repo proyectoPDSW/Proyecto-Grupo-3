d3707f8266f96a689bb1df276ce51e97118e387d
Hugo Alvarez <hugoferneya@hotmail.com>
2016-05-10 11:38:04 -0500

    encontrando errores con la devolucion

--- src/main/java/edu/eci/pdsw/managedbeans/RegistroDevolucionManageBean.java
+++ src/main/java/edu/eci/pdsw/managedbeans/RegistroDevolucionManageBean.java
@@ -8,11 +8,14 @@ package edu.eci.pdsw.managedbeans;
 import edu.eci.pdsw.entities.EquipoSencillo;
 import edu.eci.pdsw.entities.Prestamo;
 import edu.eci.pdsw.entities.EquipoComplejo;
+import edu.eci.pdsw.entities.Persona;
 import edu.eci.pdsw.servicios.ServiciosEquipoComplejo;
 import edu.eci.pdsw.servicios.ServiciosEquipoComplejoPersistence;
 import edu.eci.pdsw.servicios.ExcepcionServicios;
 import edu.eci.pdsw.servicios.ServiciosPrestamo;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.faces.application.FacesMessage;
@@ -26,7 +29,7 @@ import javax.faces.context.FacesContext;
  */
 @ManagedBean(name = "RegistroDevolucion")
 @SessionScoped
-public class RegistroDevolucionManageBean {
+public class RegistroDevolucionManageBean implements Serializable{
     private final ServiciosPrestamo PRESTAMO;
     private final ServiciosEquipoComplejo EQCOMPLEJO;
     private List<EquipoSencillo> es;
@@ -38,9 +41,22 @@ public class RegistroDevolucionManageBean {
     private String eqcompl;
     private String selectEqSe;
     private int cantidad;
+    private Persona elQuePideElPrestamo;
+    private Set<EquipoComplejo> equiposComplejosPrestados;
+    private Set<EquipoComplejo> equiposComplejosFaltantes;
+    
+    private boolean showPanelInfoPrestamista=false;
+    
+    
+    public boolean showPanelInfo(){
+        return showPanelInfoPrestamista;
+    }
+    
     public RegistroDevolucionManageBean() {
         PRESTAMO = ServiciosPrestamo.getInstance();
         EQCOMPLEJO = ServiciosEquipoComplejoPersistence.getInstance();
+        equiposComplejosPrestados=new HashSet<>();
+        equiposComplejosFaltantes=new HashSet<>();
     }
     
     public List<String> mostrarListaEquipoSencillo() {
@@ -50,6 +66,7 @@ public class RegistroDevolucionManageBean {
             List<Prestamo> p = PRESTAMO.consultarPrestamosPersona(laPersona);
             for (Prestamo p1 : p) {
                 if (p1.getFechaRealEntregada() == null) {
+                    
                     for (EquipoSencillo es1 : p1.getEquiposSencillosFaltantes()) {
                         es.add(es1);
                     }
@@ -83,23 +100,37 @@ public class RegistroDevolucionManageBean {
 
     public void registroDevolucionEquipoComplejo() {
         try {
+            boolean check=false;
             EquipoComplejo eqcomp = EQCOMPLEJO.consultarPorPlaca(placa);
             List<Prestamo> prestamos = PRESTAMO.consultarPrestamosEquipoComplejo(eqcomp);
             for (Prestamo p1 : prestamos) {
+                list<Prestamo> otros = PRESTAMO.consultarPrestamosPersona(placa);
                 if (!p1.terminado()) {
+                    
+                    System.out.println("ERSDGFSDGSDF");
                     Set<EquipoComplejo> ecp = p1.getEquiposComplejosFaltantes();
+                    
                     for (EquipoComplejo ecp1 : ecp) {
+                        System.out.println(ecp1.toString() +"\n"+eqcomp.toString());
                         if (ecp1.equals(eqcomp)) {
-                            laPersona = p1.getElQuePideElPrestamo().getNombre();
+                            check=true;
+                            elQuePideElPrestamo = p1.getElQuePideElPrestamo();
+                            equiposComplejosPrestados=p1.getEquiposComplejosPrestados();
+                            equiposComplejosFaltantes=p1.getEquiposComplejosFaltantes();
                         }
                     }
                 }
             }
             eqcompl = eqcomp.getModelo_Eq().getNombre();
             PRESTAMO.registrarDevolucion(placa);
-            facesInfo("Se realizo con exito la devolución");
+            if(check) {
+                showPanelInfoPrestamista=true;
+                facesInfo("Se realizo con exito la devolución");
+            }
+            else facesError("El equipo no esta prestado");
         } catch (ExcepcionServicios ex) {
             facesError(ex.getMessage());
+            showPanelInfoPrestamista=false;
         }
     }
 
@@ -165,6 +196,38 @@ public class RegistroDevolucionManageBean {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public Persona getElQuePideElPrestamo() {
+        return elQuePideElPrestamo;
+    }
+
+    public void setElQuePideElPrestamo(Persona elQuePideElPrestamo) {
+        this.elQuePideElPrestamo = elQuePideElPrestamo;
+    }
+
+    public boolean isShowPanelInfoPrestamista() {
+        return showPanelInfoPrestamista;
+    }
+
+    public void setShowPanelInfoPrestamista(boolean showPanelInfoPrestamista) {
+        this.showPanelInfoPrestamista = showPanelInfoPrestamista;
+    }
+
+    public Set<EquipoComplejo> getEquiposComplejosPrestados() {
+        return equiposComplejosPrestados;
+    }
+
+    public void setEquiposComplejosPrestados(Set<EquipoComplejo> equiposComplejosPrestados) {
+        this.equiposComplejosPrestados = equiposComplejosPrestados;
+    }
+
+    public Set<EquipoComplejo> getEquiposComplejosFaltantes() {
+        return equiposComplejosFaltantes;
+    }
+
+    public void setEquiposComplejosFaltantes(Set<EquipoComplejo> equiposComplejosFaltantes) {
+        this.equiposComplejosFaltantes = equiposComplejosFaltantes;
     }
     
     /**
