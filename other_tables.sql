99c4eeea0dc7ddd5efb9d4d6ea8d9bcad2b77ae6
Julian Devia <julian.devia@mail.escuelaing.edu.co>
2016-05-20 16:14:52 -0500

    correcciones sql con codigo orden de compra y codigo activo

--- other_tables.sql
+++ other_tables.sql
@@ -66,6 +66,8 @@ CREATE TABLE Informacion_Compra (
     fecha_garantia date NOT NULL,
     Equipos_Complejos_serial varchar(30)   NOT NULL,
     Equipos_Complejos_modelo varchar(50)   NOT NULL,
+    codigo_orden_compra varchar(100) NOT NULL,
+    codigo_activo varchar(100) NOT NULL,
     CONSTRAINT Informacion_Compra_pk PRIMARY KEY (Equipos_Complejos_serial,Equipos_Complejos_modelo)
 )ENGINE=InnoDB  ;
 
